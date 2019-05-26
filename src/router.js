cd4e657b961c60b8f7e48456c393a62baeeace25
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-05-26 16:30:58 -0400

    added routes for part one which just respond with 200 okay

--- src/router.js
+++ src/router.js
@@ -6,15 +6,43 @@ const db = require('./db');
 const router = express.Router();
 
 router.post('/register', (req, res, next) => {
-  const newUser = {
-    userID: req.body.id,
-    passwordHash: req.body.hash,
-  };
+  res.sendStatus(200)
+    .next();
+  //const newUser = {
+  //  userID: req.body.id,
+  //  passwordHash: req.body.hash,
+  //};
   //write new user to db asyncronously
   //.then response.status(200)
   //.catch
 });
 
-router.post('/login', (res, req, next) => {
-  //TODO
-})
+router.post('/login', (req, res, next) => {
+  res.sendStatus(200)
+    .next();
+});
+
+router.get('/all-prod', (req, res, next) => {
+  res.sendStatus(200)
+    .next();
+});
+
+router.get('/product/:id', (req, res, next) => {
+  res.sendStatus(200)
+    .next();
+});
+
+router.post('/product/', (req, res, next) => {
+  res.sendStatus(200)
+    .next();
+});
+
+router.put('/product/:id', (req, res, next) => {
+  res.sendStatus(200)
+    .next();
+});
+
+router.delete('/product/:id', (req, res, next) => {
+  res.sendStatus(200)
+    .next();
+});
