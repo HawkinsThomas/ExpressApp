fae756475a79acb7d48cb7da4e8f3ce5631b6806
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-06-30 12:54:24 -0400

    had to manually revert to an old commit... still learning how to use git revert properly

--- src/db/index.js
+++ src/db/index.js
@@ -1,77 +1,72 @@
+'use strict';
+
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
 
-let readFile = util.promisify(fs.readFile);
+const readFile = util.promisify(fs.readFile);
 //let writeFile = util.promisify(fs.writeFile);
 
 const productsPath = path.resolve('./src/db/product.json');
+const usersPath = path.resolve('./src/db/users.json');
 
 const register = (newUser) => {
-  fs.readFile('./src/db/users.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    else {
+  readFile(usersPath)
+    .then((data) => {
       const users = JSON.parse(data);
       if (users[Object.keys(newUser)[0]]){
         console.log("user already exists");
         return
       }
       updatedUsers = {...users, ...newUser};
-      fs.writeFile('./src/db/users.json', JSON.stringify(updatedUsers, null, 2), 'utf8', (err) => {
+      fs.writeFile(usersPath, JSON.stringify(updatedUsers, null, 2), 'utf8', (err) => {
         if (err) throw err;
       });
-    }
   });
 };
 
 const createProduct = (newProduct) => {
-  fs.readFile('./src/db/product.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    else {
+  readFile(productsPath)
+    .then((data) => {
       const products = JSON.parse(data);
       if (products[Object.keys(newProduct)[0]]){
         console.log('product already exists');
         return;
       }
       updatedProducts = {...products, ...newProduct};
-      fs.writeFile('./src/db/product.json', JSON.stringify(updatedProducts, null, 2), 'utf8', (err) => {
+      fs.writeFile(productsPath, JSON.stringify(updatedProducts, null, 2), 'utf8', (err) => {
         if (err) throw err;
       });
-    }
   });
-};
+}
 
 const updateProduct = (newProduct) => {
-  fs.readFile('./src/db/product.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    else {
+  readFile(productsPath)
+    .then((data) => {
       const products = JSON.parse(data);
       if (!(products[Object.keys(newProduct)[0]])){
         console.log('product does not exist ');
         return;
       }
       updatedProducts = {...products, ...newProduct};
-      fs.writeFile('./src/db/product.json', JSON.stringify(updatedProducts, null, 2), 'utf8', (err) => {
+      fs.writeFile(productsPath, JSON.stringify(updatedProducts, null, 2), 'utf8', (err) => {
         if (err) throw err;
       });
-    }
   });
 };
 
 const deleteProduct = (product) => {
-  fs.readFile('./src/db/product.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    else {
+  readFile(productsPath)
+    .then((data) => {
       const products = JSON.parse(data);
       if (!(products[product])){
         console.log('product does not exist');
         return;
       }
       delete products[product];
-      fs.writeFile('./src/db/product.json', JSON.stringify(products, null, 2), 'utf8', (err) => {
+      fs.writeFile(productsPath, JSON.stringify(products, null, 2), 'utf8', (err) => {
         if (err) throw err;
       });
-    }
   });
 }
 
